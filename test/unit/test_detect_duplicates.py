fe2c580ffbfe0f1d09130fb529b65acc6173fd65
lohengrin <oljn22@student.bth.se>
2025-06-02 16:06:13 +0200

    Add test

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,29 @@
 import pytest
+from unittest.mock import patch, MagicMock
+from src.util.detector import detect_duplicates
 
-# develop your test cases here
+pytestmark = pytest.mark.unit
 
-@pytest.mark.unit
-def test_detect_duplicates():
-    assert True
+@pytest.fixture
+@patch('src.util.detector.parse', autospec=True)
+@patch('src.util.detector.Article', autospec=True)
+def sut_one_entry(mock_article, mock_parse):
+    mock_article.return_value = None
+    mock_parse.return_value = [mock_article]
+    return detect_duplicates
+
+# @pytest.fixture
+# @patch('src.util.detector.parse', autospec=True)
+# @patch('src.util.detector.Article', autospec=True)
+# def sut_two_entries(mock_article, mock_parse):
+#     mock_article.return_value = None
+#     mock_parse.return_value = [mock_article, mock_article]
+#     return detect_duplicates
+
+# TC 1
+def test_one_entry(sut_one_entry):
+    """ Should raise ValueError """
+    with pytest.raises(ValueError):
+        result = sut_one_entry(data=None)
+
+# TC 2
