d6ba58b7217757b63df33aeb2bf2e5155b425ddb
lohengrin <oljn22@student.bth.se>
2025-06-02 16:59:21 +0200

    Improve

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -4,13 +4,28 @@ from src.util.detector import detect_duplicates
 
 pytestmark = pytest.mark.unit
 
+# @pytest.fixture
+# @patch('src.util.detector.parse', autospec=True)
+# @patch('src.util.detector.Article', autospec=True)
+# def sut_one_entry(mock_article, mock_parse):
+#     mock_article.return_value = None
+#     mock_parse.return_value = [mock_article]
+#     return detect_duplicates
+
 @pytest.fixture
-@patch('src.util.detector.parse', autospec=True)
-@patch('src.util.detector.Article', autospec=True)
-def sut_one_entry(mock_article, mock_parse):
-    mock_article.return_value = None
-    mock_parse.return_value = [mock_article]
-    return detect_duplicates
+def sut():
+    def _sut(parse_return_val):
+        with patch('src.util.detector.parse', autospec=True) as mock_parse:
+            mock_parse.return_value = parse_return_val
+            return detect_duplicates(data=None)
+    return _sut
+
+# TC 1
+def test_1(sut):
+    with pytest.raises(ValueError):
+        sut(["one bibtex entry"])
+
+
 
 # @pytest.fixture
 # @patch('src.util.detector.parse', autospec=True)
@@ -26,15 +41,14 @@ def sut_one_entry(mock_article, mock_parse):
 #     with pytest.raises(ValueError):
 #         result = sut_one_entry(data=None)
 
-@patch('src.util.detector.parse', autospec=True)
-@patch('src.util.detector.Article', autospec=True)
-def test_one_entry(mock_article, mock_parse):
-    """ Should raise ValueError """
-    mock_article.return_value = None
-    # mock_parse.return_value = [mock_article]
-    mock_parse.return_value = []
-    with pytest.raises(ValueError):
-        result = detect_duplicates(data=None)
+# @patch('src.util.detector.parse', autospec=True)
+# @patch('src.util.detector.Article', autospec=True)
+# def test_one_entry(mock_article, mock_parse):
+#     """ Should raise ValueError """
+#     mock_article.return_value = None
+#     mock_parse.return_value = [mock_article]
+#     with pytest.raises(ValueError):
+#         result = detect_duplicates(data=None)
 
 
 # TC 2
